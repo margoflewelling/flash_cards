c7b749cdb7f934135a98731c3c91493d864030dd
Margo Flewelling <flewelling.margo@gmail.com>
2020-01-29 08:49:28 -0700

    Fix counting cards in each category method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,5 +1,5 @@
 class Deck
-attr_reader :cards, :category
+attr_reader :cards
 
   def initialize(cards)
     @cards = cards
@@ -10,12 +10,12 @@ attr_reader :cards, :category
   end
 
   def cards_in_category(cat)
-    @cat = cat
-  cards.each do |card.category|
-    if card.category = @cat
+    category_count = 0
+  @cards.each do |card|
+    if card.category == cat
   category_count += 1
-  category_count
     end
   end
+  category_count
   end
 end
