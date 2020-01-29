3050dafd249768258f9ec9123dad2e2b782bde11
Margo Flewelling <flewelling.margo@gmail.com>
2020-01-28 21:35:21 -0700

    start cards_in_cateogry method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,5 +1,5 @@
 class Deck
-attr_reader :cards
+attr_reader :cards, :category
 
   def initialize(cards)
     @cards = cards
@@ -7,5 +7,15 @@ attr_reader :cards
 
   def count
     cards.count
+  end
+
+  def cards_in_category(cat)
+    @cat = cat
+  cards.each do |card.category|
+    if card.category = @cat
+  category_count += 1
+  category_count
+    end
+  end
   end
 end
