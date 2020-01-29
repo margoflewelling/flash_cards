f819b93f9837ec56a867850a7b5888b29fb21e74
Margo Flewelling <flewelling.margo@gmail.com>
2020-01-28 18:36:13 -0700

    All test but feedback working

---
+++ test/turn_test.rb
@@ -0,0 +1,54 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/turn'
+
+class TurnTest < Minitest::Test
+
+  def test_it_exists
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    assert_instance_of Card, card
+  end
+
+  def test_turn_exists
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    assert_instance_of Turn, turn
+  end
+
+  def test_turn_card
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    assert_equal turn.card, card
+  end
+
+  def test_it_has_a_guess
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    turn.guess
+   assert_equal turn.guess, "Juneau"
+  end
+
+
+   def test_correct
+     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+     turn = Turn.new("Juneau", card)
+     turn.correct?
+     assert_equal turn.correct?, true
+   end
+
+   def test_feedback
+     skip
+     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+     turn = Turn.new("Juneau", card)
+     turn.feedback
+    assert_equal turn.feedback, "Correct!"
+  end
+
+end
+
+
+#
+# pry(main)> turn.feedback
+# #=> "Correct!"
+#
