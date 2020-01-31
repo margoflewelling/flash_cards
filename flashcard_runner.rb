7bf01a59ae88a6533281675a49cb97ee7712e137
Margo Flewelling <flewelling.margo@gmail.com>
2020-01-31 12:50:54 -0700

    Work on start method

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -8,6 +8,7 @@ require './lib/round'
 # Put those card into a Deck
 # Create a new Round using the Deck you created
 # Start the round using a new method called start
+def start
 
 card_1 = Card.new("What does Phoebe eat?", "Trash", :Phoebe)
 card_2 = Card.new("What color sweater does Odie look best in?", "Green", :Odie)
@@ -18,13 +19,18 @@ round = Round.new(deck)
 
 puts "Welcome! You're playing with #{cards.count} cards."
 puts "------------------------------------------"
-puts "This is card number 1 out of #{cards.count}."
+total_cards = cards.count
 
+ deck.cards.each do |card|
+  puts "This is card number x out of #{total_cards}"
+ puts card.question
+ guess = gets.chomp
+ new_turn = round.take_turn(guess)
+ puts new_turn.feedback
+ end
 
-def start
-puts @current_card.question
-guess = gets.chomp
-take_turn(guess)
+ puts "***** Game Over! *****"
+ puts "You had correct guesses out of #{total_cards} for a total score of #{round.percent_correct}."
 end
 
 start
