40b5fba4e59a92ea5c8b2e1e30e32a257be7b6b4
Margo Flewelling <flewelling.margo@gmail.com>
2020-01-31 18:21:34 -0700

    Change .count to .length

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,19 +6,17 @@ attr_reader :cards
   end
 
   def count
-    cards.count
+    cards.length
   end
 
-
   def cards_in_category(cat)
-  cat_array = []
+   cat_array = []
     @cards.each do |card|
       if card.category == cat
         cat_array << card
       end
     end
     cat_array
-
   end
 
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,8 +5,6 @@ require './lib/deck'
 
 class DeckTest < Minitest::Test
 
-
-
   def test_it_exists
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
@@ -22,7 +20,7 @@ class DeckTest < Minitest::Test
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
-  assert_equal deck.cards, cards
+    assert_equal deck.cards, cards
    end
 
    def test_deck_count
@@ -31,7 +29,7 @@ class DeckTest < Minitest::Test
      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
      cards = [card_1, card_2, card_3]
      deck = Deck.new(cards)
-   assert_equal deck.count(), 3
+     assert_equal deck.count(), 3
     end
 
   def test_category_count
@@ -40,6 +38,8 @@ class DeckTest < Minitest::Test
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
-  assert_equal [card_2,  card_3], deck.cards_in_category(:STEM)
+    assert_equal [card_2,  card_3], deck.cards_in_category(:STEM)
+    assert_equal [card_1], deck.cards_in_category(:Geography)
    end
+
 end
