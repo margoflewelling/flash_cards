f7410de647235ee0c65de3f592ea0da02a97f9c6
Margo Flewelling <flewelling.margo@gmail.com>
2020-01-31 19:18:50 -0700

    Start is running

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -3,34 +3,42 @@ require './lib/deck'
 require './lib/turn'
 require './lib/round'
 
-
-# Create some Cards
-# Put those card into a Deck
-# Create a new Round using the Deck you created
-# Start the round using a new method called start
 def start
 
-card_1 = Card.new("What does Phoebe eat?", "Trash", :Phoebe)
-card_2 = Card.new("What color sweater does Odie look best in?", "Green", :Odie)
-card_3 = Card.new("Who was Janis named after?", "Janis Joplin", :Other)
-cards = [card_1, card_2, card_3]
-deck = Deck.new(cards)
-round = Round.new(deck)
+  card_1 = Card.new("What does Phoebe eat?", "trash", :Phoebe)
+  card_2 = Card.new("What color sweater does Odie look best in?", "green", :Odie)
+  card_3 = Card.new("Who was Janis named after?", "janis joplin", :Other)
+  cards = [card_1, card_2, card_3]
+  deck = Deck.new(cards)
+  round = Round.new(deck)
 
-puts "Welcome! You're playing with #{cards.count} cards."
-puts "------------------------------------------"
-total_cards = cards.count
+  puts "Welcome! You're playing with #{cards.count} cards."
+  puts "------------------------------------------"
+  total_cards = cards.count
 
- deck.cards.each do |card|
-  puts "This is card number x out of #{total_cards}"
- puts card.question
- guess = gets.chomp
- new_turn = round.take_turn(guess)
- puts new_turn.feedback
- end
+  all_categories = []
+  cards.each do |card|
+    all_categories << card.category
+  end
+  unique_categories = all_categories.uniq
 
- puts "***** Game Over! *****"
- puts "You had correct guesses out of #{total_cards} for a total score of #{round.percent_correct}."
+    starting = 1
+    while starting <= total_cards
+      puts "This is card number #{starting} out of #{total_cards}"
+      puts round.current_card.question
+      guess = gets.chomp
+      new_turn = round.take_turn(guess)
+      puts new_turn.feedback
+      starting += 1
+    end
+
+    puts "***** Game Over! *****"
+    puts "You had #{round.number_correct} correct guesses out of #{total_cards} for a total score of #{round.percent_correct}."
+
+  unique_categories.each do |category|
+     puts "#{category} - #{round.percent_correct_by_category(category)} correct"
+  end
+
 end
 
 start
